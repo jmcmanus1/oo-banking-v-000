a719448e6a20abca89e2616899f3b0a2a7305a18
Julie  McManus <julieannesays@gmail.com>
2020-02-13 02:13:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,12 +25,10 @@ class Transfer
   end
 
   def reverse_transfer
-    if valid? && receiver.balance > amount && sender.status = "complete"
+    if self.status == "complete"
     sender.balance = sender.balance + amount
     receiver.balance = receiver.balance - amount
     self.status = "reversed"
-    else
-      reject_transfer
     end
   end
 
