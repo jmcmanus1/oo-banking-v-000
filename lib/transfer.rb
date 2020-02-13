5b7d70af537fd366b598f089c8b0c9ba9e780e3e
Julie  McManus <julieannesays@gmail.com>
2020-02-13 02:07:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && sender.balance > amount && sender.status == "pending"
+    if valid? && sender.balance > amount && sender.status = "pending"
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       self.status = "complete"
