3c91310d03db2632d4203640fa225ed0e6847df8
Julie  McManus <julieannesays@gmail.com>
2020-02-13 01:32:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,6 +26,6 @@ class Transfer
   def reverse_transfer
     sender.balance = sender.balance + amount
     receiver.balance = receiver.balance - amount
-    self.status = "reversed
+    self.status = "reversed"
   end
 end
