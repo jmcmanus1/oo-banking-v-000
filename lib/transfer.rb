cd649851622833e7c0b52bee697cf98bbd692257
Julie  McManus <julieannesays@gmail.com>
2020-02-13 01:53:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,7 @@ class Transfer
     sender.balance = sender.balance + amount
     receiver.balance = receiver.balance - amount
     self.status = "reversed"
-    else
+    else 
       "Transaction rejected."
     end
   end
